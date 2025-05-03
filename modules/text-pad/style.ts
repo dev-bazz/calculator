0b7d8112d03f31596bc0c8c665a96c0a937d4fe0
Clement Bazuaye <clement.bazuaye@interswitchgroup.com>
2025-05-03 12:24:20 +0100

    Adjust flex value in textPadStyle for improved layout consistency

--- modules/text-pad/style.ts
+++ modules/text-pad/style.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 export const textPadStyle = (theme: Theme) =>
 	StyleSheet.create({
 		section: {
-			flex: 1.5,
+			flex: 1.3,
 			paddingInline: normalize(40),
 			backgroundColor: theme.colors.card,
 			paddingTop: normalize(48),
